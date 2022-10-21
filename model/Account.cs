fa985a00c0f60bee13110494ddac5d4fe835ad0b
HieuMai <maivanhieu19052002@gmail.com>
2022-10-21 15:40:07 +0700

    create project

---
+++ model/Account.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTL_LTTQ_NHOM3_HETHONGBANGIAY.Model
+{
+    class Account
+    {
+        private string username;
+        private string password;
+
+        public Account()
+        {
+        }
+
+        public Account(string username, string password)
+        {
+            this.username = username;
+            this.password = password;
+        }
+
+        public void setUsername(string username)
+        {
+            this.username = username;
+        }
+
+        // set passoword
+        public void setPassword(string password)
+        {
+            this.password = password;
+        }
+
+        public string getUsername()
+        {
+            return this.username;
+        }
+        public string getPassword()
+        {
+            return this.password;
+        }
+    }
+}
