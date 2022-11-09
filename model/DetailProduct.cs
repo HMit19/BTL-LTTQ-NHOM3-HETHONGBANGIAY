96d05fbfb93f2788ac4b321fca8b9ae1b319fbbf
HieuMai <maivanhieu19052002@gmail.com>
2022-11-09 13:38:26 +0700

    update model

---
+++ model/DetailProduct.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTL_LTTQ_NHOM3_HETHONGBANGIAY.model
+{
+    class DetailProduct
+    {
+        private string idDetail;
+        private string idProduct;
+        private int size;
+        private string color;
+        private double priceOut;
+        private double priceIn;
+        private bool status;
+
+        public string IdDetail { get => idDetail; set => idDetail = value; }
+        public string IdProduct { get => idProduct; set => idProduct = value; }
+        public int Size { get => size; set => size = value; }
+        public string Color { get => color; set => color = value; }
+        public double PriceOut { get => priceOut; set => priceOut = value; }
+        public double PriceIn { get => priceIn; set => priceIn = value; }
+        public bool Status { get => status; set => status = value; }
+
+        public DetailProduct() { }
+
+        public DetailProduct(string idDetail, string idProduct, int size, string color, double priceOut, double priceIn, bool status)
+        {
+            this.idDetail = idDetail;
+            this.idProduct = idProduct;
+            this.size = size;
+            this.color = color;
+            this.priceOut = priceOut;
+            this.priceIn = priceIn;
+            this.status = status;
+        }
+    }
+}
---
+++ model/Product.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTL_LTTQ_NHOM3_HETHONGBANGIAY.model
+{
+    class Product
+    {
+        private string id;
+        private string name;
+        private int image;
+        private int idCategory;
+
+        public string Id { get => id; set => id = value; }
+        public string Name { get => name; set => name = value; }
+        public int Image { get => image; set => image = value; }
+        public int IdCategory { get => idCategory; set => idCategory = value; }
+
+        public Product() { }
+        public Product(string id, string name, int image, int idCategory)
+        {
+            this.id = id;
+            this.name = name;
+            this.image = image;
+            this.idCategory = idCategory;
+        }
+
+        public Product(string id, string name, int image)
+        {
+            this.id = id;
+            this.name = name;
+            this.image = image;
+        }
+    }
+}
