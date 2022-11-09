aa8d3b8434f7194d9d6a4136fb31015bdaec6c68
HieuMai <maivanhieu19052002@gmail.com>
2022-11-10 01:24:52 +0700

    update models

---
+++ model/SellBillDetail.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTL_LTTQ_NHOM3_HETHONGBANGIAY.model
+{
+    internal class SellBillDetail
+    {
+    }
+}
